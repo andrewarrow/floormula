a9f77772a0c6b6f3ac02463e859f0fb198c6d39c
aa <oneone@gmail.com>
2025-04-28 12:33:56 -0700

    tubes

---
+++ floormula.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+@main
+struct floormulaApp: App {
+    init() {
+        // Register for any initial app setup
+        print("floormulaApp initializing")
+    }
+    
+    var body: some SwiftUI.Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
